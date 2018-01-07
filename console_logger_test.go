e307795bdab1192d1927ece24e534f45d4e34d36
Erik Davidson <erik@erikd.org>
2018-01-07 10:20:56 -0600

    Update to use sweet for testing

--- console_logger_test.go
+++ console_logger_test.go
@@ -5,15 +5,19 @@ import (
 	"time"
 
 	"github.com/aphistic/gomol"
-
-	. "gopkg.in/check.v1"
+	"github.com/aphistic/sweet"
+	. "github.com/onsi/gomega"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func TestMain(m *testing.M) {
+	RegisterFailHandler(sweet.GomegaFail)
+
+	sweet.Run(m, func(s *sweet.S) {
+		s.AddSuite(&GomolSuite{})
+	})
+}
 
 type GomolSuite struct{}
-
-var _ = Suite(&GomolSuite{})
 
 type testConsoleWriter struct {
 	Output []string
@@ -29,28 +33,28 @@ func (w *testConsoleWriter) Print(msg string) {
 	w.Output = append(w.Output, msg)
 }
 
-func (s *GomolSuite) TestTestConsoleWriter(c *C) {
+func (s *GomolSuite) TestTestConsoleWriter(t sweet.T) {
 	w := newTestConsoleWriter()
-	c.Check(w.Output, NotNil)
-	c.Check(w.Output, HasLen, 0)
+	Expect(w.Output).ToNot(BeNil())
+	Expect(w.Output).To(HaveLen(0))
 
 	w.Print("print1")
-	c.Check(w.Output, HasLen, 1)
+	Expect(w.Output).To(HaveLen(1))
 
 	w.Print("print2")
-	c.Check(w.Output, HasLen, 2)
+	Expect(w.Output).To(HaveLen(2))
 }
 
 // Issue-specific tests
 
-func (s *GomolSuite) TestIssue5StringFormatting(c *C) {
+func (s *GomolSuite) TestIssue5StringFormatting(t sweet.T) {
 	b := gomol.NewBase()
 	b.InitLoggers()
 
 	cfg := NewConsoleLoggerConfig()
 	cfg.Colorize = false
 	l, err := NewConsoleLogger(cfg)
-	c.Assert(err, IsNil)
+	Expect(err).To(BeNil())
 	w := newTestConsoleWriter()
 	l.setWriter(w)
 	b.AddLogger(l)
@@ -59,11 +63,11 @@ func (s *GomolSuite) TestIssue5StringFormatting(c *C) {
 
 	b.ShutdownLoggers()
 
-	c.Assert(w.Output, HasLen, 1)
-	c.Check(w.Output[0], Equals, "[DEBUG] msg 100%\n")
+	Expect(w.Output).To(HaveLen(1))
+	Expect(w.Output[0]).To(Equal("[DEBUG] msg 100%\n"))
 }
 
-func (s *GomolSuite) TestAttrsMergedFromBase(c *C) {
+func (s *GomolSuite) TestAttrsMergedFromBase(t sweet.T) {
 	b := gomol.NewBase()
 	b.SetAttr("base_attr", "foo")
 	b.InitLoggers()
@@ -72,11 +76,13 @@ func (s *GomolSuite) TestAttrsMergedFromBase(c *C) {
 	cfg.Colorize = false
 	l, err := NewConsoleLogger(cfg)
 
-	testTpl, _ := gomol.NewTemplate("[{{color}}{{ucase .LevelName}}{{reset}}] {{.Message}}" +
-		"{{if .Attrs}}{{range $key, $val := .Attrs}}\n   {{$key}}: {{$val}}{{end}}{{end}}")
+	testTpl, err := gomol.NewTemplate(
+		"[{{color}}{{ucase .LevelName}}{{reset}}] {{.Message}}" +
+			"{{if .Attrs}}{{range $key, $val := .Attrs}}\n   {{$key}}: {{$val}}{{end}}{{end}}",
+	)
 
 	l.SetTemplate(testTpl)
-	c.Assert(err, IsNil)
+	Expect(err).To(BeNil())
 	w := newTestConsoleWriter()
 	l.setWriter(w)
 	b.AddLogger(l)
@@ -91,52 +97,52 @@ func (s *GomolSuite) TestAttrsMergedFromBase(c *C) {
 
 	b.ShutdownLoggers()
 
-	c.Assert(w.Output, HasLen, 1)
-	c.Check(w.Output[0], Equals, "[DEBUG] msg 100%\n   adapter_attr: bar\n   base_attr: foo\n   log_attr: baz\n")
+	Expect(w.Output).To(HaveLen(1))
+	Expect(w.Output[0]).To(Equal("[DEBUG] msg 100%\n   adapter_attr: bar\n   base_attr: foo\n   log_attr: baz\n"))
 }
 
 // General tests
 
-func (s *GomolSuite) TestConsoleSetTemplate(c *C) {
+func (s *GomolSuite) TestConsoleSetTemplate(t sweet.T) {
 	cl, err := NewConsoleLogger(nil)
-	c.Check(cl.tpl, NotNil)
+	Expect(cl.tpl).ToNot(BeNil())
 
 	err = cl.SetTemplate(nil)
-	c.Check(err, NotNil)
+	Expect(err).ToNot(BeNil())
 
 	tpl, err := gomol.NewTemplate("")
-	c.Assert(err, IsNil)
+	Expect(err).To(BeNil())
 	err = cl.SetTemplate(tpl)
-	c.Check(err, IsNil)
+	Expect(err).To(BeNil())
 }
 
-func (s *GomolSuite) TestConsoleInitLogger(c *C) {
+func (s *GomolSuite) TestConsoleInitLogger(t sweet.T) {
 	cl, err := NewConsoleLogger(nil)
-	c.Assert(err, IsNil)
-	c.Check(cl.IsInitialized(), Equals, false)
+	Expect(err).To(BeNil())
+	Expect(cl.IsInitialized()).To(BeFalse())
 	cl.InitLogger()
-	c.Check(cl.IsInitialized(), Equals, true)
+	Expect(cl.IsInitialized()).To(BeTrue())
 }
 
-func (s *GomolSuite) TestConsoleShutdownLogger(c *C) {
+func (s *GomolSuite) TestConsoleShutdownLogger(t sweet.T) {
 	cl, _ := NewConsoleLogger(nil)
 	cl.InitLogger()
-	c.Check(cl.IsInitialized(), Equals, true)
+	Expect(cl.IsInitialized()).To(BeTrue())
 	cl.ShutdownLogger()
-	c.Check(cl.IsInitialized(), Equals, false)
+	Expect(cl.IsInitialized()).To(BeFalse())
 }
 
-func (s *GomolSuite) TestConsoleColorLogm(c *C) {
+func (s *GomolSuite) TestConsoleColorLogm(t sweet.T) {
 	cfg := NewConsoleLoggerConfig()
 	cl, _ := NewConsoleLogger(cfg)
 	w := newTestConsoleWriter()
 	cl.setWriter(w)
 	cl.Logm(time.Now(), gomol.LevelFatal, nil, "test")
-	c.Assert(w.Output, HasLen, 1)
-	c.Check(w.Output[0], Equals, "[\x1b[1;31mFATAL\x1b[0m] test\n")
+	Expect(w.Output).To(HaveLen(1))
+	Expect(w.Output[0]).To(Equal("[\x1b[1;31mFATAL\x1b[0m] test\n"))
 }
 
-func (s *GomolSuite) TestConsoleLogm(c *C) {
+func (s *GomolSuite) TestConsoleLogm(t sweet.T) {
 	cfg := NewConsoleLoggerConfig()
 	cfg.Colorize = false
 	cl, _ := NewConsoleLogger(cfg)
@@ -149,11 +155,11 @@ func (s *GomolSuite) TestConsoleLogm(c *C) {
 			"attr1": 4321,
 		},
 		"test 1234")
-	c.Assert(w.Output, HasLen, 1)
-	c.Check(w.Output[0], Equals, "[FATAL] test 1234\n")
+	Expect(w.Output).To(HaveLen(1))
+	Expect(w.Output[0]).To(Equal("[FATAL] test 1234\n"))
 }
 
-func (s *GomolSuite) TestConsoleBaseAttrs(c *C) {
+func (s *GomolSuite) TestConsoleBaseAttrs(t sweet.T) {
 	b := gomol.NewBase()
 	b.SetAttr("attr1", 7890)
 	b.SetAttr("attr2", "val2")
@@ -172,6 +178,6 @@ func (s *GomolSuite) TestConsoleBaseAttrs(c *C) {
 			"attr3": "val3",
 		},
 		"test 1234")
-	c.Assert(w.Output, HasLen, 1)
-	c.Check(w.Output[0], Equals, "[DEBUG] test 1234\n")
+	Expect(w.Output).To(HaveLen(1))
+	Expect(w.Output[0]).To(Equal("[DEBUG] test 1234\n"))
 }
